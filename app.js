5a12d292e222e6355a0e47edae354ee01867da94
Tasleem Ahmad Naeem <tasleem.a.n.official@gmail.com>
2025-02-25 22:04:18 +0500

    new commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const morgan = require("morgan");
 const tourRouter = require("./dev-data/routes/tourRoutes");
 const userRouter = require("./dev-data/routes/userRoutes");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
@@ -16,17 +18,19 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// defining a global middleware for error handling
+app.use(globalErrorHandler);
 
 module.exports = app;
 
